12f31dfa084fac43251544729c255c0ea7e27a9e
Glagny <nikoicardo@gmail.com>
2021-10-07 13:26:24 -0600

    Add POST tech functionality.

--- routes/techs.js
+++ routes/techs.js
@@ -2,13 +2,37 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
-// @route   POST api/logs
-// @desc    Enter a New Log
+const Tech = require('../models/Tech');
+
+
+
+// @route   POST api/techs
+// @desc    Enter a New tech
 // @access  Public
 
-router.post('/', (req, res) => {
-  console.log('You made it!');
-  res.json({message: 'You Made It!'});
-});
+router.post(
+  '/',
+  [
+    check('firstName', 'Please add a first name.').not().isEmpty(),
+    check('lastName', 'Please add a last name.').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(500).json(errors);
+    }
+
+    try {
+      let tech = new Tech(req.body);
+
+      await tech.save();
+
+      res.status(200).json({ msg: 'Tech Added.' });
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
